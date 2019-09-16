08517e17fec73869da5b5bcd8d8075b392817737
sagits <renatopprobst@gmail.com>
2019-09-15 22:40:39 -0300

    adding topbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@material/react-top-app-bar/dist/top-app-bar.css';
+import '@material/react-material-icon/dist/material-icon.css';
+import TopAppBar, {
+    TopAppBarFixedAdjust,
+    TopAppBarIcon,
+    TopAppBarRow,
+    TopAppBarSection,
+    TopAppBarTitle,
+} from '@material/react-top-app-bar';
+import MaterialIcon from '@material/react-material-icon';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <div>
+          <TopAppBar>
+              <TopAppBarRow>
+                  <TopAppBarSection align='start'>
+                      <TopAppBarIcon navIcon tabIndex={0}>
+                          <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')}/>
+                      </TopAppBarIcon>
+                      <TopAppBarTitle>I loved your new haircut</TopAppBarTitle>
+                  </TopAppBarSection>
+                  <TopAppBarSection align='end' role='toolbar'>
+                      <TopAppBarIcon actionItem tabIndex={0}>
+                          <MaterialIcon
+                              aria-label="print page"
+                              hasRipple
+                              icon='print'
+                              onClick={() => console.log('print')}
+                          />
+                      </TopAppBarIcon>
+                  </TopAppBarSection>
+              </TopAppBarRow>
+          </TopAppBar>
+          <TopAppBarFixedAdjust>
+              My exciting content!
+          </TopAppBarFixedAdjust>
+      </div>
   );
 }
 
