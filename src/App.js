8efad56f24f3d3ab8f130f1877cbb5aa856bea22
Ana <anacarolina.gerhardt@hotmail.com>
2019-09-18 01:21:03 -0300

    inserido bootstrap e container com dropdowns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import '@material/react-top-app-bar/dist/top-app-bar.css';
 import '@material/react-material-icon/dist/material-icon.css';
 import TopAppBar, {
@@ -13,34 +15,56 @@ import TopAppBar, {
 import MaterialIcon from '@material/react-material-icon';
 
 
-
 function App() {
   return (
-      <div>
-          <TopAppBar>
-              <TopAppBarRow>
-                  <TopAppBarSection align='start'>
-                      <TopAppBarIcon navIcon tabIndex={0}>
-                          <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')}/>
-                      </TopAppBarIcon>
-                      <TopAppBarTitle>I loved your new haircut</TopAppBarTitle>
-                  </TopAppBarSection>
-                  <TopAppBarSection align='end' role='toolbar'>
-                      <TopAppBarIcon actionItem tabIndex={0}>
-                          <MaterialIcon
-                              aria-label="print page"
-                              hasRipple
-                              icon='print'
-                              onClick={() => console.log('print')}
-                          />
-                      </TopAppBarIcon>
-                  </TopAppBarSection>
-              </TopAppBarRow>
-          </TopAppBar>
-          <TopAppBarFixedAdjust>
-              My exciting content!
-          </TopAppBarFixedAdjust>
-      </div>
+    <div className='container-fluid'>
+        <TopAppBar>
+            <TopAppBarRow>
+                <TopAppBarSection align='start'>
+                    <TopAppBarIcon navIcon tabIndex={0}>
+                        <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')}/>
+                    </TopAppBarIcon>
+                    <TopAppBarTitle>Google Translate</TopAppBarTitle>
+                </TopAppBarSection>
+                {/* <TopAppBarSection align='end' role='toolbar'>
+                    <TopAppBarIcon actionItem tabIndex={0}>
+                        <MaterialIcon
+                            aria-label="print page"
+                            hasRipple
+                            icon='print'
+                            onClick={() => console.log('print')}
+                        />
+                    </TopAppBarIcon>
+                </TopAppBarSection> */}
+            </TopAppBarRow>
+        </TopAppBar>
+        <TopAppBarFixedAdjust></TopAppBarFixedAdjust>
+
+            <div className='row align-items-center' id='selectors-box'>
+
+                    <div className='col-5 p-2' id='selectors-btn' align='center'>
+                        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </DropdownButton>
+                    </div>
+
+                    <div className='col-2 p-3' align='center'>
+                            <button type="button" class="btn btn-primary btn-sm" id="toggle-btn"><i class="material-icons">compare_arrows</i></button>
+                    </div>
+
+                    <div className='col-5' align='center'>
+                        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </DropdownButton>
+                    </div>
+
+            </div>
+
+    </div>
   );
 }
 
