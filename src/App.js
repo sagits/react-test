96ffb6cf1a2416963dc39b4e07ea54081b0cced8
Ana <anacarolina.gerhardt@hotmail.com>
2019-09-20 03:52:45 -0300

    mediaquery ok, inserido input form (tentando alterar o css)

--- src/App.js
+++ src/App.js
@@ -15,62 +15,95 @@ import TopAppBar, {
 } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
 
-function App() {
-    return (
-        <div>
-            <TopAppBar>
-                <TopAppBarRow>
-                    <TopAppBarSection align='start'>
-                        <TopAppBarIcon navIcon tabIndex={0}>
-                            <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')} />
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 'Please write an essay about your favorite DOM element.'
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('An essay was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render () {
+        return (
+            <div>
+                <TopAppBar>
+                    <TopAppBarRow>
+                        <TopAppBarSection align='start'>
+                            <TopAppBarIcon navIcon tabIndex={0}>
+                                <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')} />
+                            </TopAppBarIcon>
+                            <TopAppBarTitle>Google Translate</TopAppBarTitle>
+                        </TopAppBarSection>
+                        {/* <TopAppBarSection align='end' role='toolbar'>
+                        <TopAppBarIcon actionItem tabIndex={0}>
+                            <MaterialIcon
+                                aria-label="print page"
+                                hasRipple
+                                icon='print'
+                                onClick={() => console.log('print')}
+                            />
                         </TopAppBarIcon>
-                        <TopAppBarTitle>Google Translate</TopAppBarTitle>
-                    </TopAppBarSection>
-                    {/* <TopAppBarSection align='end' role='toolbar'>
-                    <TopAppBarIcon actionItem tabIndex={0}>
-                        <MaterialIcon
-                            aria-label="print page"
-                            hasRipple
-                            icon='print'
-                            onClick={() => console.log('print')}
-                        />
-                    </TopAppBarIcon>
-                </TopAppBarSection> */}
-                </TopAppBarRow>
-            </TopAppBar>
-            <TopAppBarFixedAdjust>
-                <div className='container-fluid'>
-                    <div className='row align-items-center' id='selectors-box'>
+                    </TopAppBarSection> */}
+                    </TopAppBarRow>
+                </TopAppBar>
 
-                        <div className='col-5 p-2' id='selectors-btn' align='center'>
-                            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            </DropdownButton>
+                {/* Content */}
+                <TopAppBarFixedAdjust>
+                    <div className='container-fluid'>
+
+                        {/* Dropdowns Box */}
+                        <div className='row align-items-center' id='dropdowns-box'>
+                            <div className='col-5' align='center'>   
+                                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
+
+                            <div className='col-2' align='center'>
+                                <Button variant="primary" size="sm" className="toggle-btn">
+                                    <i className="material-icons">compare_arrows</i>
+                                </Button>
+                            </div>
+
+                            <div className='col-5' align='center'>
+                                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
                         </div>
 
-                        <div className='col-2 p-3' align='center'>
-                            <Button variant="primary" size="sm" className="toggle-btn">
-                                <i className="material-icons">compare_arrows</i>
-                            </Button>
-                        </div>
-
-                        <div className='col-5 p-0' align='center'>
-                            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            </DropdownButton>
-                        </div>
+                        {/* Input Box */}
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                                <textarea value={this.state.value} onChange={this.handleChange} />
+                            </label>
+                            <input className="invisible" type="submit" value="Submit" />
+                        </form>
 
                     </div>
-                </div>
 
-            </TopAppBarFixedAdjust>
-        </div>
+                </TopAppBarFixedAdjust>
+            </div>
 
-    );
+        );
+    }
 }
 
 export default App;
